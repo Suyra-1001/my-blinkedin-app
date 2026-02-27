bca17e0d7eee3f62947e2b768b979e2e08c92526
Suraj Kumar <ksuraj86103@gmail.com>
2026-02-27 10:57:11 +0530

    Reorganize imports and improve code structure

--- project.py
+++ project.py
@@ -248,7 +248,7 @@ def handle_msg(data):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    socketio.run(app, host='0.0.0.0',port=10000)
+    socketio.run(app, host='0.0.0.0', port=10000)
 
 
 
